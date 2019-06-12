75365acbf231dfeb68f72b31f4d353d0bf64dfef
teedty1 <andy.castillo@sturgiswebservices.com>
2019-06-12 07:59:25 -0400

    getting closer to angular working

--- www/Data/_script/main.ts
+++ www/Data/_script/main.ts
@@ -1,1 +1,7 @@
-﻿console.log("Main");
+﻿import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
